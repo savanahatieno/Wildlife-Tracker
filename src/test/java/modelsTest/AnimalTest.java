75c25ec76938e896b2732fd2d2c94cac5ace5338
savanahatieno <savanaht01@gmail.com>
2021-04-28 14:06:14 +0300

    add test

--- src/test/java/modelsTest/AnimalTest.java
+++ src/test/java/modelsTest/AnimalTest.java
@@ -4,7 +4,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import org.junit.Assert.*;
 import org.sql2o.*;
+import static org.junit.Assert.assertEquals;
 
 public class AnimalTest {
 
+
+
+
+    @Test
+    public void animal_instatiatesCorrectly_false() {
+        AnimalTest testAnimal = new AnimalTest("");
+        assertEquals(true, testAnimal instanceof  AnimalTest);
+    }
 }
