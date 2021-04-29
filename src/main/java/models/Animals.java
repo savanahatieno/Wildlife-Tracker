d5f340a30a5191ed309b2b17437d8404b86a8411
savanahatieno <savanaht01@gmail.com>
2021-04-29 13:12:26 +0300

    created querys to display some database details

--- src/main/java/models/Animals.java
+++ src/main/java/models/Animals.java
@@ -1,6 +1,7 @@
 package models;
 import models.BasicInterface;
 
+import org.h2.value.ValueStringIgnoreCase;
 import org.sql2o.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -34,16 +35,7 @@ public class Animals  {
     }
 
 
-    //where one can enter details and is able to save/update it
-    public void save() {
-        try(Connection connection = DB.sql2o.open()) {
-            String sql = "INSERT INTO animals (name) VALUES (:name);";
-            this.id = (int) connection.createQuery(sql, true)
-                    .addParameter("name", this.name)
-                    .executeUpdate()
-                    .getKey();
-        }
-    }
+
 
     //where one can view all the selected animals
     public static  List<Animals> all() {
@@ -64,6 +56,50 @@ public class Animals  {
                return animals;
        }
     }
+
+    //where one can enter details and is able to save/update it
+    public void save() {
+        try(Connection connection = DB.sql2o.open()) {
+            String sql = "INSERT INTO animals (name) VALUES (:name);";
+            this.id = (int) connection.createQuery(sql, true)
+                    .addParameter("name", this.name)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+
+    //where the user can update the database
+    public void updateTheNames (String name) {
+       try(Connection connection = DB.sql2o.open()){
+           String sql = "UPDATE animals SET name=:name WHERE id=:id;";
+           connection.createQuery(sql)
+                   .addParameter("name", name)
+                   .addParameter("id", id)
+                   .executeUpdate();
+       }
+    }
+
+    //where one can delete an animal in case of wrong details
+    public void dropAnimal() {
+       try (Connection connection = DB.sql2o.open()) {
+           String sql = "DROP FROM animals WHERE id = :id;";
+           connection.createQuery(sql)
+                   .addParameter("id", id)
+                   .executeUpdate();
+       }
+    }
+
+
+    //where one can get a list of all the sightings
+     public List<Sighting> getAllSightings() {
+       try(Connection connection = DB.sql2o.open()) {
+           String sql = "SELECT * FROM sightings WHERE animals_id =:id;";
+           List<Sighting> sightings = connection.createQuery(sql)
+                   .addParameter("id", id)
+                   .executeAndFetch(Sighting.class);
+           return sightings;
+       }
+     }
 
 
 
