9593d653ff76996aefe28bc225d4d32a5b5e704d
savanahatieno <savanaht01@gmail.com>
2021-04-29 19:47:04 +0300

    added the basic test Sighting

--- src/main/java/models/Sighting.java
+++ src/main/java/models/Sighting.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Date;
 
 
-import static org.junit.Assert.*;
 
 
 public class Sighting {
--- src/test/java/modelsTest/SightingTest.java
+++ src/test/java/modelsTest/SightingTest.java
@@ -1,4 +1,28 @@
 package modelsTest;
 
+import models.Animals;
+import models.Sighting;
+import org.sql2o.*;
+import org.junit.*;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Date;
+
+import  static org.junit.Assert.*;
+
+
 public class SightingTest {
+
+    //The basic test
+
+
+    @Test
+    public void sighting_instantiatesCorrectly_true() {
+        Animals testAnimals = new Animals("Lion", 3);
+        testAnimals.save();
+        Sighting testSighting = new Sighting(testAnimals.getId(),"1.2545,36.8141","Ranger Luke");
+        assertEquals(true, testSighting instanceof Sighting);
+    }
+
+
 }
